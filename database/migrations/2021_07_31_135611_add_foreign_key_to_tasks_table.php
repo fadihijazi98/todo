27160be62cf0d12ef4fa490c0cc11442791f8973
FadiH <fadihijazi2018@outlook.com>
2021-07-31 16:57:05 +0300

    add foreign key to tasks table

---
+++ database/migrations/2021_07_31_135611_add_foreign_key_to_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->foreign('board_id')->on('boards')->references('id')
+                ->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+}
