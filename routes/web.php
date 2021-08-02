fcfac765adf328e8affe17804610b1c721d00056
FadiH <fadihijazi2018@outlook.com>
2021-08-02 12:15:21 +0300

    auth middleware

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    if (\Illuminate\Support\Facades\Auth::user())
+        return redirect('/home');
+
     return view('welcome');
 });
 
-Route::resource('board', \App\Http\Controllers\BoardController::class);
-Route::post('/board/search', [\App\Http\Controllers\BoardController::class, 'search']);
+Route::middleware('auth')->group(function () {
+    Route::resource('board', \App\Http\Controllers\BoardController::class);
+    Route::post('/board/search', [\App\Http\Controllers\BoardController::class, 'search']);
+
+    Route::resource('task', \App\Http\Controllers\TaskController::class);
+    Route::put('/task/{task}/completed', [\App\Http\Controllers\TaskController::class, 'markTaskAsCompleted']);
+    Route::put('/task/{task}/pending', [\App\Http\Controllers\TaskController::class, 'markTaskAsPending']);
+    Route::post('/task/search', [\App\Http\Controllers\TaskController::class, 'search']);
+
+    Auth::routes();
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+Auth::routes();
+
 Route::get('/board/share/{id}', [\App\Http\Controllers\BoardController::class, 'renderShareBoard']);
 
-Route::resource('task', \App\Http\Controllers\TaskController::class);
-Route::put('/task/{task}/completed', [\App\Http\Controllers\TaskController::class, 'markTaskAsCompleted']);
-Route::put('/task/{task}/pending', [\App\Http\Controllers\TaskController::class, 'markTaskAsPending']);
-Route::post('/task/search', [\App\Http\Controllers\TaskController::class, 'search']);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
